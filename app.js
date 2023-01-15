cf603dc78efff1a275e014b0ae08013aff98020d
Art Ramirez <artramirez08@gmail.com>
2023-01-15 13:32:59 -0800

    added comments to js file for things that need to be done

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 document.getElementById('start').addEventListener('click', ()=> {
     console.log('lol')
 })
+
+//keep track of time stretch goal maybe?
+
+//keep track of score
+
+//function to start game
+
+//alert for win w/ play again?
+
+//alert for lost w/ play again?
+
+//check for correct answer
+
+//how do i change questions and answers
+    //maybe change text.content?
+
+//how do i store the questions and optional answers?
