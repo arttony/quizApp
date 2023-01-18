af5692b15755bc68fadd3bbb99cac91ef0f0d092
Art Ramirez <artramirez08@gmail.com>
2023-01-17 19:03:05 -0800

    starting now displays first question

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-
-
 //keep track of time stretch goal maybe?
 
 //keep track of score
-const score = 0
+const score = 0;
+
 //function to start game
-const startBttn = document.getElementById('start')
-    startBttn.addEventListener('click', ()=> {
-    console.log('lol')
-    console.log(question1.question)
-    const gameContainer = document.createElement('div')
-    gameContainer.setAttribute('id', 'game-container')
-    gameContainer.append('AYYYYOOO')
-    const body = document.querySelector('body')
-    body.appendChild(gameContainer)
-    startBttn.remove()
-    })
+const startBttn = document.getElementById("start");
+startBttn.addEventListener("click", () => {
+  console.log("lol");
+  //console.log(question1.question);
+  const gameContainer = document.createElement("div");
+  gameContainer.setAttribute("id", "game-container");
+  gameContainer.append("AYYYYOOO");
+  const body = document.querySelector("body");
+  body.appendChild(gameContainer);
+  const questionContainer = document.createElement('div')
+  questionContainer.setAttribute('id', 'question-container')
+  const question = document.createElement('p')
+  question.setAttribute('id', 'question')
+  question.append(question1.question)
+  questionContainer.appendChild(question)
+  const options = document.createElement('ul')
+  const option1 = document.createElement('li')
+  gameContainer.appendChild(questionContainer)
+  startBttn.remove();
+});
+
 //alert for win w/ play again?
 
 //alert for lost w/ play again?
@@ -23,13 +32,13 @@ const startBttn = document.getElementById('start')
 //check for correct answer
 
 //how do i change questions and answers
-    //maybe change text.content?
+//maybe change text.content?
 
 //how do i store the questions and optional answers?
 const question1 = {
-    question: 'What time is it',
-    option1: 'yo',
-    option2: 'nope',
-    option3: 'okay',
-    option4: 'lol'
-}
+  question: "What time is it?",
+  option1: "yo",
+  option2: "nope",
+  option3: "okay",
+  option4: "lol",
+};
