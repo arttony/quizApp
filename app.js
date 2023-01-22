c9f6ab5af4ca503b14f164ed64e511e04ac540fe
Art Ramirez <artramirez08@gmail.com>
2023-01-22 12:11:05 -0800

    broken bu TODO: iterate through the questions correctly

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
+//question class
+class Question {
+
+  /* constructor(question, ans1, ans2, ans3, ans4) {
+    this.question = question;
+    this.answer1 = ans1
+    this.answer2 = ans2
+    this.answer3 = ans3
+    this.answer4 = ans4
+  } */
+
+  constructor(obj){
+    Object.assign(this, obj);
+  }
+  
+}
+
 const gameContainter = document.querySelector("main");
 const questionContainer = document.getElementById("question");
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
 const option4 = document.getElementById("option4");
+const questions = [
+  {
+    question: "What time is it?",
+    answer1: "yo",
+    answer2: "nope",
+    answer3: "okay",
+    answer4: "lol",
+    correct: 4,
+  },
+
+  {
+    question: "Capitol of France",
+    answer1: "England",
+    answer2: "New York",
+    answer3: "Germany",
+    answer4: "Paris",
+    correct: 4,
+  },
+
+  {
+    question: "What is the name of the smallest country in the world?",
+    answer1: "England",
+    answer2: "The Vatican City",
+    answer3: "Germany",
+    answer4: "Paris",
+    correct: 2,
+  },
+];
 
 //keep track of score
 let score = 0;
@@ -47,45 +92,54 @@ startBttn.addEventListener("click", () => {
   options.append(option1, option2, option3, option4)
   questionContainer.appendChild(options)
   gameContainer.appendChild(questionContainer) */
-  startBttn.setAttribute("class", "hidden");
+  startBttn.classList.add('hidden');
   gameContainter.classList.remove("hidden");
-  changeQuestion(question[questionIndex])
+  changeQuestion(questions[questionIndex])
 });
 
 //function to change question
-function changeQuestion (question[questionIndex]) {
-  questionIndex++
-  questionContainer.textContent = question[questionIndex].question
-  option1.textContent = question[questionIndex].answer1;
-  option2.textContent = question[questionIndex].answer2;
-  option3.textContent = question[questionIndex].answer3;
-  option4.textContent = question[questionIndex].answer4;
+function changeQuestion (question) {
+  //resetBackground()
+  if(let =questionIndex < questions.length){
+    questionIndex++
+    questionContainer.textContent = question.question
+    option1.textContent = question.answer1;
+    option2.textContent = question.answer2;
+    option3.textContent = question.answer3;
+    option4.textContent = question.answer4;
+  }else{
+    alert("yo")
+  }
+  
 }
 
-const question1 = {
-  question: "What time is it?",
-  answer1: "yo",
-  answer2: "nope",
-  answer3: "okay",
-  answer4: "lol",
-};
+//check for correct answer
+function checkAnswer(buttonNumber){
+  if(questions[questionIndex].correct == buttonNumber){
+    console.log(questions[questionIndex]);
+    /* document.body.classList.remove("normal"); */
+    score++
+    console.log(score)
+    changeQuestion(questions[questionIndex]);
+  } else{
+    score--
+    console.log(score)
+    /* document.body.classList.remove("normal")
+    document.body.classList.add('wrong') */
+  }
+}
 
-const question2 = {
-  question: "Capitol of France",
-  answer1: "England",
-  answer2: "New York",
-  answer3: "Germany",
-  answer4: "Paris",
-};
-
-
+/* function resetBackground(){
+  document.body.classList.add("normal");
+  document.body.classList.remove("wrong");
+} */
 //keep track of time stretch goal maybe?
 
 //alert for win w/ play again?
 
 //alert for lost w/ play again?
 
-//check for correct answer
+
 
 //how do i change questions and answers
 //maybe change text.content?
